ae2442043abf78e7ecfc592b7d30da8b3aa112da
Maxime Lelievre <maxime.lelievre@epfl.ch>
2025-10-20 23:58:29 +0300

    first commit on branch yoruba ep with results on CDPK science with english prompt

--- src/cdpk/benchmark_answers.py
+++ src/cdpk/benchmark_answers.py
@@ -40,15 +40,18 @@ def format_example(
 def gen_prompt(
     df, example_rows, question_row, question_col, choice_cols, choices, answer_col
 ):
-    prompt = "Eyi ni àwọn àpẹẹrẹ ìbéèrè olópọ̀ yíyàn (pẹ̀lú àwọn ìdáhùn).\n\n"
+    # add intro for bi-lingual prompt (Instructions in english and questions in target language)
+    prompt = "The following instructions are in English, but the example questions and the final question to be answered are in Yoruba.\n\n"
+
+    prompt += "The following are example multiple choice questions (with answers).\n\n"
     for i in example_rows:
         prompt += format_example(df, i, question_col, choice_cols, choices, answer_col)
-    prompt += "Dáhùn ìbéèrè gidi tí ó tẹ̀lé e ní lílo irú ọ̀nà ìdáhùn kan náà:\n"
+    prompt += "Answer the following real question using same answer format: \n"
     prompt += format_questions(df, question_row, question_col, choice_cols, choices)
     prompt += (
-        "\n\nDáhùn ìbéèrè gidi nìkan."
-        "\n\nPèsè lẹ́tà fún ìdáhùn rẹ nìkan."
-        "\n\nDúró gẹ́lẹ́ lẹ́yìn lẹ́tà náà."
+        "\n\nOnly answer the real question."
+        "\n\nOnly provide the letter for your answer."
+        "\n\nStop exactly after the letter."
         # "\nDo not provide any explanation."
         # "\nDo not provide any text at all other than the letter by itself."
     )
