b347af8e96fd2b22bbcbc120fd7fc61ebdc18a9e
Maxime Lelievre <maxime.lelievre@epfl.ch>
2025-11-06 19:18:55 +0300

    pushed new data for swahili, with latency per question and tokens count and added provider specification

--- src/cdpk/benchmark_answers.py
+++ src/cdpk/benchmark_answers.py
@@ -101,8 +101,55 @@ def evaluate_model(test_df, config, model, verbose=0):
     answers = test_df.loc[~example_filt].iloc[:, answer_col]
     resps = list()
     success = list()
+    extra_fields = list()
+    extra_fields_vars = [
+        "Latency",
+        "TokensUsed",
+        "TokensUsedCompletion",
+        "TokensUsedReasoning",
+    ]
 
-    extra_body = {}
+    ## Select specific provider ##
+    # DeepSeek
+    if model == "or-deepseek-2-5" or model == "deepseek-r1":
+        extra_body = {"provider": {"order": ["DeepSeek"], "allow_fallbacks": False}}
+    #NovitaAI
+    if model == "deepseek-r1-nvt" or model == "minimax-m1" or model == "kimi-k2":
+        extra_body = {"provider": {"order": ["NovitaAI"], "allow_fallbacks": False}}
+    # DeepInfra
+    if model in ["qwen-qwq-32b",
+                 "deepseek-v3-0324",
+                 "llama-v3p2-1b-instruct-bf16",
+                 "llama-v3p2-3b-instruct-bf16",
+                 "mistral-nemo-instruct-2407-fp8",
+                 "qwen-2p5-7b-instruct-bf16",
+                 "mistral-small-24b-instruct-2501-fp8",
+                 "qwen3-235b-a22b-thinking-2507",
+                 "gemma-3-4b-it",
+                 ]:
+        extra_body = {"provider": {"order": ["DeepInfra"], "allow_fallbacks": False}}
+    if model.startswith("qwen-3-") and model not in ["qwen-3-30b-a3b", "qwen-3-235b-a22b"]:
+        extra_body = {"provider": {"order": ["DeepInfra"], "allow_fallbacks": False}}
+    # Parasail
+    if model in ["gemma-3-27b",
+                 "olmo-2-32b",
+                 "qwen-3-235b-a22b-2507-bf16",
+                 ]:
+        extra_body = {"provider": {"order": ["Parasail"], "allow_fallbacks": False}}
+    # Mistral
+    if model =="mistral-small-3-1-24b" or model == "mistral-small-3p2-24b-instruct":
+        extra_body = {"provider": {"order": ["Mistral"], "allow_fallbacks": False}}
+    # xAI
+    if model in ["grok-3-beta",
+                 "grok-3-mini-beta",
+                 ]:
+        extra_body = {"provider": {"order": ["xAI"], "allow_fallbacks": False}}
+    # Lambda
+    if model == "deepseek-r1-0528-fp8-lambda":
+        extra_body = {"provider": {"order": ["Lambda"], "allow_fallbacks": False}}
+    else:
+        extra_body = {}
+
 
     for rowi in tqdm(range(len(example_rows), total_row)):
 
@@ -117,33 +164,38 @@ def evaluate_model(test_df, config, model, verbose=0):
         )
         try:
             msg = LLMMessage(Role="user", Message=prompt)
-            if (
-                model.startswith("o1-")
-                or model.startswith("o3-mini")
-                or model.startswith("claude-3-7-sonnet-20250219-thinking-")
-                or model.startswith("o4-mini")
-                or model.startswith("o3-")
-                or model.startswith("gpt-5-")
-            ):
+            prefixes = ("o1-",
+                        "o3-mini",
+                        "claude-3-7-sonnet-20250219-thinking-",
+                        "o4-mini",
+                        "o3-",
+                        "claude-sonnet-4-20250514-low",
+                        "claude-opus-4-20250514-low",
+                        "gpt-5",
+                        "claude-sonnet-4-5-20250929-low")
+            if any(model.startswith(p) for p in prefixes):
                 temperature = 1
             else:
                 temperature = 0
             if extra_body:
                 response = caller.call(
                     msg, max_tokens=None, temperature=temperature, extra_body=extra_body
-                ).Message
+                )
             else:
-                response = caller.call(
-                    msg, max_tokens=None, temperature=temperature
-                ).Message
+                response = caller.call(msg, max_tokens=None, temperature=temperature)
             if verbose > 0:
-                print(response)
-            resps.append(response)
+                print(response.Message)
+            resps.append(response.Message)
             success.append(True)
+            extra_fields_dict = {
+                key: getattr(response, key, None) for key in extra_fields_vars
+            }
+            extra_fields.append(extra_fields_dict)
         except Exception as e:
             print(e)
             resps.append("")
             success.append(False)
+            extra_fields_dict.append({})
 
         if model == "hunyuan-large-longcontext":
             time.sleep(5)
@@ -152,4 +204,4 @@ def evaluate_model(test_df, config, model, verbose=0):
         if model == "gemini-2.5-pro-exp-03-25":
             time.sleep(15)
 
-    return answers, resps, success
+    return answers, resps, extra_fields, success
