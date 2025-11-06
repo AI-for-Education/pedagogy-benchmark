f407e902109dfefd5ecc20efbff99154fd1a1db0
Maxime Lelievre <maxime.lelievre@epfl.ch>
2025-11-06 22:55:21 +0300

    ran new Hausa CDPK science

--- scripts/prepare_cdpk_dataset_multilingual.py
+++ scripts/prepare_cdpk_dataset_multilingual.py
@@ -261,7 +261,7 @@ for i in few_shot_examples_idx_dict['Science']:
     print("-----")
 # %%
 
-language = "Hausa"
+language = "Hausa_new"
 
 create_subcsv_cdpk(cdpk_dataset, 
                    folder_path = f"./../data/{language}/CDPK_per_category",
--- scripts/run_pedagogy_benchmark_multilingual.py
+++ scripts/run_pedagogy_benchmark_multilingual.py
@@ -13,7 +13,7 @@ from cdpk.benchmark_utils import fulldf_accuracy_by_category
 
 load_dotenv(override=True)
 
-language = "Hausa" # Specify the target language here, translate prompt if needed
+language = "Hausa_new" # Specify the target language here, translate prompt if needed
 
 QUESTIONS_LIST_DICT = {
     "cdpk": [
