73beac5905e5f5e97a75d6d9bdf7af5e18858344
Maxime Lelievre <maxime.lelievre@epfl.ch>
2025-11-07 13:14:58 +0300

    ran new Yoruba ep science

--- scripts/run_pedagogy_benchmark_multilingual.py
+++ scripts/run_pedagogy_benchmark_multilingual.py
@@ -13,7 +13,7 @@ from cdpk.benchmark_utils import fulldf_accuracy_by_category
 
 load_dotenv(override=True)
 
-language = "Yoruba_ep" # Specify the target language here
+language = "Yoruba_ep_new" # Specify the target language here
 
 QUESTIONS_LIST_DICT = {
     "cdpk": [
