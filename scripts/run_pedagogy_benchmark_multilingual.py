3908d322c36e778576b81622770564ff7af89118
Maxime Lelievre <maxime.lelievre@epfl.ch>
2025-12-17 15:57:37 +0300

    ran Gemini 3 Pro and GPT-5.2 on Hausa Science

--- scripts/run_pedagogy_benchmark_multilingual.py
+++ scripts/run_pedagogy_benchmark_multilingual.py
@@ -30,7 +30,7 @@ def main(opt):
     if opt.models_config is None:
         if opt.benchmark == "cdpk":
             #opt.models_config = "cdpk_online_leaderboard"
-            opt.models_config = "full_list_20251015"
+            opt.models_config = "full_list_20251015_small"
         elif opt.benchmark == "send":
             opt.models_config = "send_online_leaderboard"
         else:
